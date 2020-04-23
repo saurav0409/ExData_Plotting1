642ac10a4586730fd246b9bf2a25faaf5e0801bd
Saurav Agrawal <62747988+saurav0409@users.noreply.github.com>
2020-04-23 21:06:58 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,17 @@
+library("data.table")
+
+setwd("~/Desktop/coursera/dataanalysi/data")
+
+pow <- data.table::fread(input = "household_power_consumption.txt" , na.strings="?" )
+
+pow[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+pow[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+pow <- pow[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot2.png", width=480, height=480)
+
+plot(x = pow[, dateTime] , y = pow[, Global_active_power] , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
