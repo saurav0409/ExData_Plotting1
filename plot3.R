db362d9926e9c5bd0831260bce547a17538342ed
Saurav Agrawal <62747988+saurav0409@users.noreply.github.com>
2020-04-23 22:00:05 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -2,9 +2,7 @@ library("data.table")
 
 setwd("~/Desktop/coursera/dataanalysis/data")
 
-power<- data.table::fread(input = "household_power_consumption.txt"
-                             , na.strings="?"
-)
+power<- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
 
 power[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
@@ -15,12 +13,9 @@ power <- power[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 png("plot3.png", width=480, height=480)
 
 # Plot 3
-plot(power[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
-lines(power[, dateTime], powerDT[, Sub_metering_2],col="red")
-lines(power[, dateTime], powerDT[, Sub_metering_3],col="blue")
-legend("topright"
-       , col=c("black","red","blue")
-       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
-       ,lty=c(1,1), lwd=c(1,1))
+plot(power[, dateTime], power[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(power[, dateTime], power[, Sub_metering_2],col="red")
+lines(power[, dateTime], power[, Sub_metering_3],col="blue")
+legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
 
 dev.off()
